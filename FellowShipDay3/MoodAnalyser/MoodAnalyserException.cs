784118a07d81ed7efd53c8af726785a7fd3f28a4
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-24 17:33:14 +0530

    [Priyansh]Added:custom exception for invalid message

---
+++ FellowShipDay3/MoodAnalyser/MoodAnalyserException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserException : Exception
+    {
+
+        public enum ExceptionType 
+        {
+
+            INVALID_MOOD_EXCEPTION
+        }
+        public ExceptionType type;
+        public MoodAnalyserException(string message,ExceptionType type)
+            : base(message) { }
+    }
+}
